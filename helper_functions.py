e8dc67154713d6c68cc229a19930be17c2c17978
Erakla <social@die-genossen.de>
2021-03-17 17:47:29 +0100

    timestamp_to_unix() fixed

--- helper_functions.py
+++ helper_functions.py
@@ -5,7 +5,6 @@ def unix_to_timestamp(value: float):
     return time.strftime("%%Y-%%m-%%dT%%H:%%M:%06.3fZ" % (value % 60), time.gmtime(value))
 
 def timestamp_to_unix(timestamp: str):
-    timezonedif = time.mktime(time.gmtime(86400)) - 86400
     localtimeseconds = time.mktime(time.strptime(timestamp[:-5], "%Y-%m-%dT%H:%M:%S"))
     ms = float(timestamp[-5:-1])
-    return localtimeseconds + timezonedif + ms
+    return localtimeseconds + ms
